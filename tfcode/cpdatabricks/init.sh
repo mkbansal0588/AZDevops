1cce56414d82af942c3ffef1be47c2bddd60f4b9
Mohit Bansal <cambansal0588@gmail.com>
2021-12-29 13:34:20 -0500

    updates

--- tfcode/cpdatabricks/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Quoting the label (i.e. EOF) with single quotes to disable variable interpolation.
-cat << 'EOF' > /databricks/driver/conf/00-custom-spark.conf
-[driver] {
-  "spark.driver.extraJavaOptions" = "-Dlog4j2.formatMsgNoLookups=true"
-  "spark.executor.extraJavaOptions" = "-Dlog4j2.formatMsgNoLookups=true"
-}
-EOF
--- tfcode/cpdatabricks/removeJndiClass.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-for f in $(find /databricks -name '*log4j-core*.jar' 2>/dev/null); do echo "Checking $f..."; zip -q -d $f org/apache/logging/log4j/core/lookup/JndiLookup.class; done
